f59ff8b66ca2cbc03f4e96e097fd2e01d9877525
jeanjosephgeorge <jean.joseph@outlook.com>
2019-03-06 13:22:16 -0600

    changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+import "assets/scss/black-dashboard-react.scss";
+import "assets/css/nucleo-icons.css";
+
 import { createMuiTheme } from "@material-ui/core/styles";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 
