2779d09c165e8068d6452297f7dd1611eb5175a4
hirosoft40 <hirosoft40@gmail.com>
2019-03-07 17:37:25 -0600

    API done

--- src/App.js
+++ src/App.js
@@ -1,44 +1,74 @@
 import React, { Component } from "react";
 import "./App.css";
-import TopButtons from "./components/TopButtons";
 import Graph from "./components/Graph";
-let header = [],
-  figures = [];
-let results;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results1: []
+    };
+    this.connectToApi = this.connectToApi.bind(this);
+  }
+
   connectToApi() {
     var exampleSocket = new WebSocket("ws://rdsfastrack.com/backend/", [
       "com.campbellsci.webdata"
     ]);
 
-    exampleSocket.addEventListener("open", function(event) {
+    exampleSocket.addEventListener("open", event => {
       console.log("Hello Server!");
       exampleSocket.send(
         '{"message":"AddRequests","requests":[{"uri":"LNDB:8782_Flowback","mode":"most-recent","p1":"10","transaction":1,"order":"collected"}]}'
       );
     });
 
-    exampleSocket.addEventListener("message", function(mEvent) {
-      results = JSON.parse(mEvent.data);
-      // header = mEvent.data[0];
-      // figures = mEvent.records.data;
+    // ***SIMPLE CALL***
+    exampleSocket.addEventListener("message", async mEvent => {
+      let results = [],
+        results1 = [];
+
+      results = await JSON.parse(mEvent.data);
+      if (results.message === "RequestRecords") {
+        results1.push(results.records.data);
+        this.setState({
+          results1: results1
+        });
+      }
     });
-    return results;
+
+    // const promise1 = new Promise((resolve, reject) => {
+    //   exampleSocket.addEventListener("open", event => {
+    //     console.log("Hello Server!");
+    //     exampleSocket.send(
+    //       '{"message":"AddRequests","requests":[{"uri":"LNDB:8782_Flowback","mode":"most-recent","p1":"10","transaction":1,"order":"collected"}]}'
+    //     );
+    //   });
+
+    //   exampleSocket.addEventListener("message", mEvent => {
+    //     // if (mEvent.data.message === "RequestRecords") {
+    //     resolve(mEvent.data);
+    //     // console.log(mEvent.data)
+    //     // setTimeout(resolve(JSON.parse(mEvent.data)), 1000);
+    //     // }
+    //   });
+    // });
+    // promise1.then(value => {
+    //   console.log("value", value);
+    //   this.setState({
+    //     results1: value
+    //   });
+    // });
   }
 
   componentDidMount() {
-    const data = this.connectToApi();
-    console.log(data);
+    this.connectToApi();
   }
 
   render() {
-    console.log(results);
-    return (
-      <div>
-        <Graph />
-      </div>
-    );
+    console.log(this.state.results1);
+    // if(resutls1)
+    return <div>test</div>;
   }
 }
 
