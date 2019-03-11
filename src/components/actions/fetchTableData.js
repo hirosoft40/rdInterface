80259d9f536942cf8aba8a8b024c9c2c54708e61
hirosoft40 <hirosoft40@gmail.com>
2019-03-10 23:10:51 -0500

    reducer and index.js

---
+++ src/components/actions/fetchTableData.js
@@ -0,0 +1,34 @@
+export const fetchTableData = item => {
+  return {
+    type: "FETCH_TABLE_DATA",
+    header: item.header,
+    finaldata: item
+  };
+};
+
+  export const connectToApi= ()=>{
+  var exampleSocket = new WebSocket('ws://rdsfastrack.com/backend/', ['com.campbellsci.webdata'])
+
+      exampleSocket.addEventListener('open',  (event)=> {
+      console.log('Hello Server!')
+     exampleSocket.send('{"message":"AddRequests","requests":[{"uri":"LNDB:8782_Hour_Table","mode":"since-record","p1":"1","transaction":1,"order":"collected"}]}')
+  })
+  
+// ***SIMPLE CALL***
+  
+    exampleSocket.addEventListener('message', async  (mEvent)=> {  
+    // const results= await JSON.parse(mEvent.data).records.data
+    const results =await JSON.parse(mEvent.data);
+    if(results.message==="RequestRecords"){
+      this.setState({
+         finalData:results.records.data
+      })
+    }
+    else{
+      //seting header info
+      this.setState({
+        header:results.head.fields
+      });
+    }
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-
 import App from "./App";
-
+import BaseLayout from "./BaseLayout";
+import EnhancedTable from "./components/Tables/EnhancedTable";
+import FetchData from "./components/reducers/FetchData";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { createMuiTheme } from "@material-ui/core/styles";
 import { MuiThemeProvider } from "@material-ui/core/styles";
-import BaseLayout from "./BaseLayout";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import EnhancedTable from "./components/Tables/EnhancedTable";
+import thunk from "redux-thunk";
+
+const store = createStore(
+  FetchData,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 const theme = createMuiTheme({
   typography: {
@@ -21,15 +26,17 @@ const theme = createMuiTheme({
 });
 
 ReactDOM.render(
-  <BrowserRouter>
-    <MuiThemeProvider theme={theme}>
-      <BaseLayout>
-        <Switch>
-          <Route exact path="/" component={App} />
-          <Route path="/table" component={EnhancedTable} />
-        </Switch>
-      </BaseLayout>
-    </MuiThemeProvider>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <MuiThemeProvider theme={theme}>
+        <BaseLayout>
+          <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/table" component={EnhancedTable} />
+          </Switch>
+        </BaseLayout>
+      </MuiThemeProvider>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
