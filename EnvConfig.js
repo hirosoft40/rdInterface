6f86b23b8b6624d47b62b8c9cfe4df6a7097e648
jeanjosephgeorge <jean.joseph@outlook.com>
2019-03-13 14:47:58 -0500

    add EnvConfig file

---
+++ EnvConfig.js
@@ -0,0 +1,5 @@
+export const url = "ws://rdsfastrack.com/backend/"
+export const urlArg2 = "com.campbellsci.webdata"
+export const reqChartData = '{"message":"AddRequests","requests":[{"uri":"Server:9664_FBM_(A).Flowback","mode":"backfill","p1":"100","transaction":1,"order":"collected"}]}'
+export const serverName = 'Server: 9664_FBM_(A)'
+export const reqTableData = '{"message":"AddRequests","requests":[{"uri":"LNDB:8782_Hour_Table","mode":"since-record","p1":"1","transaction":1,"order":"collected"}]}'
